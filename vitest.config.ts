6c6f9c5622ffc068e7b5c3a781d50ca98ee50e2a
Bogdan Dolin <d.bogdan@inbox.ru>
2024-04-11 12:38:46 +0700

    vitest: add `filePArallelism: false` to make tests less flaky

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,10 @@ import { BASE_API_URL } from './lib/constants'
 
 export default defineVitestConfig ({
   test: {
+    fileParallelism: false,
     environmentOptions: {
       nuxt: {
+        domEnvironment: 'happy-dom',
         overrides: {
           app: {
             // baseURL: BASE_API_URL,
